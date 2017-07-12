be61ac70cb4cc005cee02fbefbfda794995f149a
Lewis <lewisburns630@googlemail.com>
2017-07-12 11:52:15 +0100

    Add app that displays basic message

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+window.addEventListener('load', function () {
+  ReactDOM.render(
+    <h3> Yo I'm a Message </h3>,
+    document.getElementById('app')
+  );
+});
